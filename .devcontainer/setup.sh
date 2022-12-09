00c3882ca1b4ff94aeef289ad4b4709613c76f44
DiaxuDev <63294962+DiaxManPl@users.noreply.github.com>
2022-12-09 09:41:18 +0100

    YAF

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,22 +12,8 @@ sudo apt-get install -y \
   wget \
   unzip
   
-
-fonts_dir="${HOME}/.local/share/fonts"
-if [ ! -d "${fonts_dir}" ]; then
-    echo "mkdir -p $fonts_dir"
-    mkdir -p "${fonts_dir}"
-else
-    echo "Found fonts dir $fonts_dir"
-fi
-
-version=6.2
-curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/Fira_Code_v${version}.zip --output ${zip}
-unzip -o -q -d ${fonts_dir} ${zip}
-rm ${zip}
-
-echo "fc-cache -f"
-fc-cache -f
+sudo add-apt-repository universe
+sudo apt install fonts-firacode
 
 sudo apt-get autoremove -y
 sudo apt-get autoremove -y
