8bd6dd3da0a68a1049e1e1fb7c6fc2e3b5a58e7f
DiaxuDev <63294962+DiaxManPl@users.noreply.github.com>
2022-12-09 08:38:00 +0100

    Yet antoher fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,18 +7,11 @@ sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
   curl \
   git \
-  gnupg2 \
-  jq \
   sudo \
   openssh-client \
-  less \
-  iproute2 \
-  procps \
   wget \
-  unzip \
-  apt-transport-https \
-  lsb-release
-
+  unzip
+  
 wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
 sudo unzip Fira_Code_v6.2.zip -d /usr/share/fonts
 sudo fc-cache -f -v
